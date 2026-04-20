341be3cfaa6229877d2e87f98c72d93027affce2
oangsa <sukruangkul.aongsa@gmail.com>
2026-04-20 21:44:31 +0700

    feat: add repair request item status api

--- app/api/repairRequestItemStatus.api.ts
+++ app/api/repairRequestItemStatus.api.ts
@@ -1,5 +1,5 @@
 import { http, httpPaginated } from "./http";
-import type { IPagedResult, IRepairRequestItemStatus, ISearchRequest } from "./types/types";
+import type { IPagedResult, IRepairRequestItemStatus, ISearchRequest, IRepairRequestItemStatusForCreate, IRepairRequestItemStatusForUpdate, IDeleteCollectionRequest } from "./types/types";
 
 const PREFIX = "/api/v1/repair-request-item-status";
 
@@ -11,7 +11,38 @@ export async function searchRepairRequestItemStatusesRequest(body: ISearchReques
     });
 }
 
-export async function getRepairStatusByIdRequest(id: number): Promise<IRepairRequestItemStatus>
+export async function getRepairRequestItemStatusByIdRequest(id: number): Promise<IRepairRequestItemStatus>
 {
     return http<IRepairRequestItemStatus>(`${PREFIX}/${id}`);
+}
+
+export async function createRepairRequestItemStatusRequest(body: IRepairRequestItemStatusForCreate): Promise<IRepairRequestItemStatus>
+{
+    return http<IRepairRequestItemStatus>(PREFIX, {
+        method: "POST",
+        body: JSON.stringify(body),
+    });
+}
+
+export async function updateRepairRequestItemStatusRequest(id: number, body: IRepairRequestItemStatusForUpdate): Promise<IRepairRequestItemStatus>
+{
+    return http<IRepairRequestItemStatus>(`${PREFIX}/${id}`, {
+        method: "PUT",
+        body: JSON.stringify(body),
+    });
+}
+
+export async function deleteRepairRequestItemStatusRequest(id: number): Promise<void>
+{
+    return http<void>(`${PREFIX}/${id}`, {
+        method: "DELETE",
+    });
+}
+
+export async function deleteRepairRequestItemStatusCollectionRequest(body: IDeleteCollectionRequest): Promise<void>
+{
+    return http<void>(`${PREFIX}/collection`, {
+        method: "DELETE",
+        body: JSON.stringify(body),
+    });
 }
