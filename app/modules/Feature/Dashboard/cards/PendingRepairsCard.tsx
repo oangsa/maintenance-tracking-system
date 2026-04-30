c6961e38e6508502dd83772b9237b580a721326f
oangsa <sukruangkul.aongsa@gmail.com>
2026-04-30 20:56:00 +0700

    feat: wiring pending repair request card

--- app/modules/Feature/Dashboard/cards/PendingRepairsCard.tsx
+++ app/modules/Feature/Dashboard/cards/PendingRepairsCard.tsx
@@ -1,12 +1,38 @@
 import type { IDashboardCardComponentProps } from "../types";
 import MetricSummaryValue from "./MetricSummaryValue";
+import { useEffect, useState } from "react";
+import { searchRepairRequests } from "@/services/repairRequests.service";
+import type { ISearchRequest } from "@/api/types/types";
 
 export default function PendingRepairsCard({}: IDashboardCardComponentProps)
 {
+    const [pendingRepairs, setPendingRepairs] = useState<number | null>(null);
+
+    useEffect(() => {
+        const fetchPendingRepairs = async () => {
+            const searchParams: ISearchRequest = {
+                pageSize: 9999,
+                pageNumber: 1,
+                search: [
+                    {
+                        name: "current_status_code",
+                        condition: "EQUAL",
+                        value: "REQUESTED",
+                    }
+                ]
+            };
+
+            const response = await searchRepairRequests(searchParams);
+            setPendingRepairs(response.pagination.totalCount);
+        };
+
+        fetchPendingRepairs();
+    }, []);
+
     return (
         <MetricSummaryValue
-            caption="Replace with API value"
-            value="12"
+            caption="Pending Repairs"
+            value={pendingRepairs?.toString() ?? "Loading..."}
         />
     );
 }
